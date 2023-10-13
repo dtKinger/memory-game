72630f9b76a0632ff5aa718939d9d527f9d68aad
Dan King <king.daniel.thomas@gmail.com>
2023-10-13 13:58:06 +0700

    Many CSS fixes: layout, colors etc.

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,8 +3,8 @@ import "../styles/game-over.css"
 function GameOver ({currentScore, onRestart}) {
 
   const howItEnded = currentScore === 5 ?
-  <div className="you-won" onClick={onRestart}>You Won with {currentScore}!</div> :
-  <div className="play-again" onClick={onRestart}>You Lose! But you scored {currentScore}</div>
+  <div className="you-won" onClick={onRestart}>You Won with {currentScore}! <br></br><br></br>Click to play again</div> :
+  <div className="play-again" onClick={onRestart}>You Lose! But you scored {currentScore}. <br></br><br></br>Click to play again</div>
 
   return (
     <>
