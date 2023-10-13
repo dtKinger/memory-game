3770fb69d40a0f362345d21808c5db61b92ebecc
Dan King <king.daniel.thomas@gmail.com>
2023-10-13 17:34:31 +0700

    Check maxCards for win not static 5

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,8 @@
 import "../styles/game-over.css"
 
-function GameOver ({currentScore, onRestart}) {
-  const howItEnded = currentScore === 5 ?
+function GameOver ({currentScore, onRestart, maxCards}) {
+
+  const howItEnded = currentScore == maxCards ?
   <div className="you-won" onClick={onRestart}>You Won with {currentScore}! <br></br><br></br>Click to play again</div> :
   <div className="play-again" onClick={onRestart}>You Lose! But you scored {currentScore}. <br></br><br></br>Click to play again</div>
 
