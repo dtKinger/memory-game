31323fbf0b862cd490568e1b248fd4716504c7dd
Dan King <king.daniel.thomas@gmail.com>
2023-10-13 12:11:13 +0700

    Remove the Quotes API that was for testing.

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Quotes } from './components/Quotes'
-import {MTGcards} from "./components/MTGcards.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
